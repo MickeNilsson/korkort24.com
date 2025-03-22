9b5f1034cb5bc51af0fcb53cd292a7ad1159cd50
Mikael Nilsson <mail@mikael-nilsson.se>
2025-03-22 08:49:20 +0100

    Create time slots in schedule

---
+++ api/schedule/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+//require 'get.php';
+
+
+require '../init.php';
+
+switch($_SERVER['REQUEST_METHOD']) {
+
+    case 'GET': 
+        
+        $quiz_o = json_decode(file_get_contents('./schedule.json')); 
+        
+        $response_o->{'data'} = $quiz_o;
+
+        break;
+
+    case 'POST': 
+        
+        $quiz_o = getPayload();
+
+        file_put_contents('./schedule.json', json_encode($quiz_o, JSON_UNESCAPED_UNICODE));
+        
+        break;
+}
+
+http_response_code(200);
+
+$response_o->{'status'} = 'ok';
+
+$response_s = json_encode($response_o, JSON_UNESCAPED_UNICODE);
+
+echo $response_s;
+
+
+
+
+
+// $response_o->{'data'} = get();
+
+// http_response_code(200);
+
+// $response_o->{'status'} = 'ok';
+
+// $response_s = json_encode($response_o, JSON_UNESCAPED_UNICODE);
+
+// echo $response_s;
--- src/components/pages/AdminPage.jsx
+++ src/components/pages/AdminPage.jsx
@@ -1,9 +1,17 @@
 import { Button, Col, Form, InputGroup, Modal, Row, Tab, Tabs } from 'react-bootstrap';
 import { useState, useEffect, useRef } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function AdminPage(props) {
 
+    const [chosenDate, setChosenDate] = useState('');
+
+    const [value, onChange] = useState(new Date());
+
     const [answers, setAnswers] = useState([]);
+
+    const [availableTimes, setAvailableTimes] = useState([]);
 
     const [quiz, setQuiz] = useState([]);
 
@@ -23,6 +31,18 @@ export default function AdminPage(props) {
 
     const [image, setImage] = useState({});
 
+    const [timeSlotLength, setTimeSlotLength] = useState('');
+
+    const [timeSlots, setTimeSlots] = useState([]);
+
+    const [startTime, setStartTime] = useState('');
+
+    const [endTime, setEndTime] = useState('');
+
+    const [events, setEvents] = useState([]);
+
+    const [schedule, setSchedule] = useState({});
+
     useEffect(() => {
 
         init();
@@ -30,14 +50,127 @@ export default function AdminPage(props) {
         async function init() {
 
             await loadQuiz();
+
+            await fetchAvailableTimes();
+
+            await loadSchedule();
         }
 
     }, []);
 
+    async function fetchAvailableTimes() {
+
+        // const availTimes = [
+        //     {
+        //         id: 1,
+        //         from: '2025-01-23T10:00:00',
+        //         to: '2025-01-23T10:30:00'
+        //     },
+        //     {
+        //         id: 2,
+        //         from: '2025-01-20T12:00:00',
+        //         to: '2025-01-20T12:30:00'
+        //     },
+        //     {
+        //         id: 3,
+        //         from: '2025-01-21T09:00:00',
+        //         to: '2025-01-21T10:00:00'
+        //     },
+        //     {
+        //         id: 4,
+        //         from: '2025-01-20T09:00:00',
+        //         to: '2025-01-20T10:30:00'
+        //     }
+        // ];
+
+        const response = await fetch('https://korkort24.com/api/times/');
+
+        let availTimes = await response.json();
+
+        //availTimes = availTimes.filter(availTime => !availTime.studentid);
+
+        availTimes.sort((a, b) => {
+            if (a.from < b.from) return -1;
+            if (a.from > b.from) return 1;
+            return 0;
+        });
+
+        let availDates = availTimes.map(availTime => availTime.from.substring(0, 10));
+
+
+        availDates = [...(new Set(availDates))];
+
+        console.log('=== availTimes ===');
+        console.log(availTimes);
+
+        console.log('=== availDates ===');
+        console.log(availDates);
+
+        setAvailableTimes(availTimes);
+
+        const eve = availDates.map(availDate => {
+
+            const year = +availDate.substring(0, 4);
+
+            let month = (+availDate.substring(5, 7));
+
+            month--;
+
+            const day = +availDate.substring(8, 10);
+
+            return new Date(year, month, day);
+        });
+
+        console.log('=== eve ===');
+        console.log(eve);
+        //setEvents(eve);
+
+        return availTimes;
+    }
+
+    const isEventDate = (date) => {
+        return events.some(
+            (eventDate) =>
+                eventDate.getFullYear() === date.getFullYear() &&
+                eventDate.getMonth() === date.getMonth() &&
+                eventDate.getDate() === date.getDate()
+        );
+    };
+
+    function handleClickDay(date) {
+
+        let chosenDate_o = new Date(date);
+
+        chosenDate_o.setDate(chosenDate_o.getDate() + 1);//.toISOString().substring(0, 10);
+
+        const chosenDate_s = chosenDate_o.toISOString().substring(0, 10);
+
+        console.log(chosenDate_s);
+
+        setChosenDate(chosenDate_s);
+
+        console.log(schedule[chosenDate_s])
+
+        // console.dir(chosenDate_s);
+
+        // console.log(availableTimes);
+
+
+        // const timeslots = availableTimes.filter(availableTime => {
+        //     console.log(availableTime.from.substring(0, 10) + ' = ' + chosenDate_s);
+        //     return availableTime.from.substring(0, 10) === chosenDate_s;
+        // });
+
+        // console.log(timeslots);
+
+        // setChosenAvailableTimes(timeslots);
+        //console.log(value);
+    }
+
     async function loadQuiz() {
 
         const response_o = await fetch('https://korkort24.com/api/quiz/');
-  
+
         if (response_o.status === 200) {
 
             const responseBody_o = await response_o.json();
@@ -145,7 +278,7 @@ export default function AdminPage(props) {
 
         let fileName_s = '';
 
-        if(image.size) {
+        if (image.size) {
             fileName_s = await uploadFile(questionId_s);
         }
 
@@ -172,7 +305,7 @@ export default function AdminPage(props) {
 
         await saveQuiz(quiz);
 
-        
+
 
         setShowAddQuestionModal(false);
     }
@@ -199,11 +332,11 @@ export default function AdminPage(props) {
 
     async function deleteQuestion(questionId_s) {
 
-        for(const category_o of quiz) {
+        for (const category_o of quiz) {
 
             category_o.questions = category_o.questions.filter(question_o => question_o.id !== questionId_s);
         }
-     
+
         setQuiz([...quiz]);
 
         await saveQuiz(quiz);
@@ -229,6 +362,131 @@ export default function AdminPage(props) {
         return fileName_s;
     }
 
+    /**
+     * Validates and saves the current time slot. Only numbers are allowed.
+     * 
+     * @param {string} timeSlot_s 
+     */
+    function validateTimeSlot(timeSlot_s) {
+
+        timeSlot_s = timeSlot_s.replace(/[^0-9]/g, '');
+
+        setTimeSlotLength(timeSlot_s);
+    }
+
+    /**
+     * Validates and saves the current start time. Only numbers are allowed. A colon is added automatically.
+     * @param {string} startTime_s 
+     */
+    function validateStartTime(startTime_s) {
+
+        startTime_s = startTime_s.replace(/[^0-9]/g, '');
+
+        if (startTime_s.length > 2) {
+            startTime_s = startTime_s.substring(0, 2) + ':' + startTime_s.substring(2, 4);
+        }
+
+        setStartTime(startTime_s);
+    }
+
+    /**
+     * Validates and saves the current end time. Only numbers are allowed. A colon is added automatically.
+     * 
+     * @param {*} endTime_s 
+     */
+    function validateEndTime(inputField_o) {
+
+        let endTime_s = inputField_o.value;
+
+        endTime_s = endTime_s.replace(/[^0-9]/g, '');
+
+        if (endTime_s.length > 2) {
+            endTime_s = endTime_s.substring(0, 2) + ':' + endTime_s.substring(2, 4);
+        }
+
+        setEndTime(endTime_s);
+    }
+
+    function createTimeSlot() {
+
+        if (+timeSlotLength && startTime.length === 5 && endTime.length === 5 && startTime < endTime && startTime > '00:00' && endTime < '23:59') {
+            const newTimeSlot_o = {
+                length: +timeSlotLength,
+                start: startTime,
+                end: endTime
+            };
+
+            setTimeSlots([...timeSlots, newTimeSlot_o]);
+
+            setTimeSlotLength('');
+
+            setStartTime('');
+
+            setEndTime('');
+        }
+    }
+
+    async function addTimeSlotToDay(timeSlot_o) {
+
+        schedule[chosenDate] = schedule[chosenDate] || [];
+
+        for(const chosenDateTimeSlot_o of schedule[chosenDate]) {
+            if(chosenDateTimeSlot_o.start === timeSlot_o.start) {
+                return;
+            }
+        }
+        schedule[chosenDate].push(timeSlot_o);
+
+        schedule[chosenDate].sort((a, b) => {
+            if(a.start < b.start) return -1;
+            if(a.start > b.start) return 1;
+            return 0
+        });
+
+        setSchedule({ ...schedule });
+
+        await saveSchedule({ ...schedule });
+    }
+
+    function deleteScheduleDate(chosenTimeSlot_o, date_s) {
+
+        const date_a = schedule[date_s].filter(timeSlot_o => JSON.stringify(chosenTimeSlot_o) !== JSON.stringify(timeSlot_o));
+        
+        schedule[date_s] = date_a;
+
+        setSchedule({ ...schedule });
+
+        saveSchedule({ ...schedule });
+    }
+
+    async function loadSchedule() {
+
+        const response_o = await fetch('https://korkort24.com/api/schedule/');
+
+        if (response_o.status === 200) {
+
+            const responseBody_o = await response_o.json();
+
+            const schedule_a = responseBody_o.data;
+
+            setSchedule(schedule_a);
+        }
+    }
+
+    async function saveSchedule(schedule_o) {
+
+        const response_o = await fetch('https://korkort24.com/api/schedule/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(schedule_o)
+        });
+
+        const responseBody_o = await response_o.json();
+    }
+
     return (
 
         <div className='pb-5'>
@@ -242,26 +500,6 @@ export default function AdminPage(props) {
             >
 
                 <Tab eventKey="questions" title="Frågor">
-
-                    {/* <Form.Group className='mb-3'>
-
-                        <Form.Control
-                            className='mb-2'
-                            onChange={(e) => setSection(e.target.value)}
-                            placeholder='Ny kategori'
-                            size='sm'
-                            spellCheck='false'
-                            type='text' />
-
-                        <Button
-                            size='sm'
-                            type='button'
-                            variant='primary'
-                            className='mb-2'
-                            onClick={addSection}
-                        >Lägg till ny kategori</Button>
-
-                    </Form.Group> */}
 
                     <Row className="align-items-center">
 
@@ -295,7 +533,7 @@ export default function AdminPage(props) {
 
                     <div className='bg-body p-2'>
                         {quiz.map(section => (
-                            <div style={{fontSize: '20px'}} key={section.name}>
+                            <div style={{ fontSize: '20px' }} key={section.name}>
                                 <span className='section-name' role='button' onClick={() => { setShowQuestionsModal(section.name) }}>{section.name}</span> <i onClick={() => { setSectionNameToDelete(section.name); setShowDeleteSectionModal(true); }} role='button' className='mx-2 bi bi-trash'></i>
                                 <i onClick={() => { setShowAddQuestionModal(section.name); setAnswers(['']); setQuestion(''); }} role='button' className='bi bi-plus-square-fill text-success'></i>
                             </div>
@@ -306,7 +544,76 @@ export default function AdminPage(props) {
 
                 <Tab eventKey="info" title="Info"></Tab>
 
-                <Tab eventKey="bookings" title="Bokningar"></Tab>
+                <Tab eventKey="bookings" title="Bokningar">
+
+                    <Row>
+                        <Col>
+                            <Form.Control
+                                onChange={(e) => validateTimeSlot(e.target.value)}
+                                placeholder='Längd (min)'
+                                size='sm'
+                                spellCheck='false'
+                                type='text'
+                                value={timeSlotLength} />
+                        </Col>
+                        <Col>
+                            <Form.Control
+                                onChange={(e) => validateStartTime(e.target.value)}
+                                placeholder='Start (hh:mm)'
+                                size='sm'
+                                spellCheck='false'
+                                type='text'
+                                value={startTime} />
+                        </Col>
+                        <Col>
+                            <Form.Control
+                                onChange={(e) => validateEndTime(e.target)}
+                                placeholder='Slut (hh:mm)'
+                                size='sm'
+                                spellCheck='false'
+                                type='text'
+                                value={endTime} />
+                        </Col>
+                        <Col>
+                            <Button
+                                onClick={() => createTimeSlot()}
+                                size='sm'
+                                variant='primary'
+                            >
+                                Skapa
+                            </Button>
+                        </Col>
+
+                    </Row>
+
+                    <div className='my-2'>
+
+                    {timeSlots.map(timeSlot_o => (
+                        <Button
+                            className='me-2'
+                            onClick={() => addTimeSlotToDay({ start: timeSlot_o.start, end: timeSlot_o.end, length: timeSlot_o.length })}
+                            size='sm'
+                            variant='primary'
+                        >
+                            {timeSlot_o.start + ' - ' + timeSlot_o.end + ' ' + timeSlot_o.length + ' min'}
+                        </Button>
+                    ))
+                    }
+                    </div>
+                    
+
+
+                    <div style={{float: 'right'}}>
+                    {schedule[chosenDate] && schedule[chosenDate].map(chosenDate_o => (
+                            <div onClick={() => deleteScheduleDate(chosenDate_o, chosenDate)} className='schedule-date' style={{cursor: 'pointer', marginBottom: '5px', backgroundColor: 'white', padding: '3px', border: '2px solid black', borderRadius: '5px'}}>{chosenDate_o.start} - {chosenDate_o.end} {chosenDate_o.length + ' min'}</div>
+                        ))}
+                    </div>
+                    
+
+                    <Calendar style={{ float: 'left' }} onChange={onChange} onClickDay={handleClickDay} value={value} tileClassName={({ date, view }) =>
+                        view === "month" && isEventDate(date) ? "highlight" : null
+                    } />
+                </Tab>
 
             </Tabs>
 
