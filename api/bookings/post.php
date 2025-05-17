601295542f1556b423cc93f751960d6e18d150f3
Mikael Nilsson <mail@mikael-nilsson.se>
2025-05-17 16:01:10 +0100

    add post to endpoint bookings

--- api/bookings/post.php
+++ api/bookings/post.php
@@ -1,16 +1,19 @@
 <?php
 
-function post($payload_a, $db_o) {
+function post($payload_o, $db_o) {
 
     $params_a = [
-        'schedule_id' => $payload_a['schedule_id'],
-        'student_id' => $payload_a['student_id'],
+        'schedule_id' => $payload_o->{'schedule_id'},
+        'member_id' => $payload_o->{'member_id'},
         'cancelled' => false,
-        'from_datetime' => date('Y-m-d H:i:s', strtotime($payload_a['from_datetime'])),
-        'to_datetime' => date('Y-m-d H:i:s', strtotime($payload_a['to_datetime']))
+        'start' => $payload_o->{'start'},
+        'end' => $payload_o->{'end'}
     ];
 
-    $result_o = $db_o->insertInto('booking', $params_a);
+    //'start' => date('Y-m-d H:i:s', strtotime($payload_a['start'])),
+    //'end' => date('Y-m-d H:i:s', strtotime($payload_a['end']))
+
+    $result_o = $db_o->insertInto('k24_booking', $params_a);
 
     return $result_o;
 }
