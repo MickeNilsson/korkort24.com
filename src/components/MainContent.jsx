96235db3d5bfb826b915f5e6cedd86cda14508d9
Mikael Nilsson <mail@mikael-nilsson.se>
2026-01-31 16:50:52 +0100

    fix: static height of background

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,17 +1,7 @@
 // Node modules
-import {useState} from 'react';
 import {Container} from 'react-bootstrap';
 
-// Components
-import AdminPage from './pages/AdminPage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import StartPage from './pages/StartPage';
-import StudentAccountPage from './pages/StudentAccountPage';
-
 export default function MainContent({page}) {
-
-    const [quiz, setQuiz] = useState(null);
 
     return (
 
@@ -22,21 +12,3 @@ export default function MainContent({page}) {
         </div>
     );
 }
-
-// function getCurrentPage({page, setPage, setStudent, student}) {
-
-//     switch(page) {
-
-//         case 'Admin account': return <AdminPage />;
-
-//         case 'Login': return <LoginPage setPage={setPage} setStudent={setStudent} />;
-
-//         case 'Quiz': return <QuizPage setPage={setPage} quiz=
-
-//         case 'Sign up': return <SignUpPage setPage={setPage} setStudent={setStudent} />;
-
-//         case 'Start': return <StartPage setPage={setPage} />;
-
-//         case 'Student account': return <StudentAccountPage setQuiz={setQuiz} setPage={setPage} student={student} />;
-//     }
-// }
